8b3510eeb60b9878066b1506e7299b7570c4bb1a
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-23 15:31:48 -0500

    refactor code to swtich between lists with a button

--- src/pages/ProfileDetails/ProfileDetails.tsx
+++ src/pages/ProfileDetails/ProfileDetails.tsx
@@ -12,6 +12,9 @@ const ProfileDetails = (): JSX.Element => {
   const { id } = useParams<{ id: string }>()
   const [profile, setProfile] = useState<Profile | null>(null)
   const [lists, setLists] = useState<List[] | null>(null)
+  const [index, setIndex] = useState(0)
+  let list
+  if (lists) list = lists[index]
 
   useEffect(() => {
     const fetchProfile = async (): Promise<void> => {
@@ -19,7 +22,7 @@ const ProfileDetails = (): JSX.Element => {
         const data: Profile = await profileService.getProfile(id)
         setProfile(data)
         setLists(data.lists)
-        console.log(data);
+        setIndex(0)
       } catch (error) {
         console.log(error);
       }
@@ -27,24 +30,32 @@ const ProfileDetails = (): JSX.Element => {
     fetchProfile()
   }, [id])
   
-
+  function handleClick(): void {
+    if (lists) index >= lists.length - 1
+    ?
+    setIndex(0)
+    :
+    setIndex(index + 1)
+  }
+  
   return (
     <>
       <h1>Profile Details</h1>
       <p>{profile?.name}</p>
       <img src={profile?.photo} alt="Profile Photo" />
-      {lists?.map(list => (
-        <div key={list.id}>
-          <p>Favorite Movie: {list.movie}</p>
-          <p>Favorite Book: {list.book}</p>
-          <p>Favorite Tv Show: {list.tvShow}</p>
-          <p>Favorite Song: {list.song}</p>
-          <p>Favorite Video Game: {list.videoGame}</p>
-          <p>Favorite Board Game: {list.boardGame}</p>
-          <p>Favorite Indoor Activity: {list.indoorActivity}</p>
-          <p>Favorite Outdoor Activity: {list.outdoorActivity}</p>
-        </div>
-      ))}
+      <button onClick={handleClick}>
+        Next List
+      </button>
+      <div>
+        <p>Favorite Movie: {list?.movie}</p>
+        <p>Favorite Book: {list?.book}</p>
+        <p>Favorite Tv Show: {list?.tvShow}</p>
+        <p>Favorite Song: {list?.song}</p>
+        <p>Favorite Video Game: {list?.videoGame}</p>
+        <p>Favorite Board Game: {list?.boardGame}</p>
+        <p>Favorite Indoor Activity: {list?.indoorActivity}</p>
+        <p>Favorite Outdoor Activity: {list?.outdoorActivity}</p>
+      </div>
     </>
   )
 }
