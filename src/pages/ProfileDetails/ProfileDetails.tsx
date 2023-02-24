1916e1d99665c1cc4f7b5fa7d1873cbf1611b62b
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-23 22:58:11 -0500

    add ternary operator to conditionally display movies

--- src/pages/ProfileDetails/ProfileDetails.tsx
+++ src/pages/ProfileDetails/ProfileDetails.tsx
@@ -33,13 +33,13 @@ const ProfileDetails = (): JSX.Element => {
   function handleClick(evt: React.MouseEvent): void {
     let btnContent: string | null = (evt.target as HTMLButtonElement).textContent
 
-    if (btnContent === 'Prev List') {
+    if (btnContent === 'Prev Movie') {
       movies && index <= 0
       ?
       setIndex(movies.length - 1)
       :
       setIndex(index - 1)
-    } else if  (btnContent === 'Next List') {
+    } else if  (btnContent === 'Next Movie') {
       movies && index >= movies.length - 1
       ?
       setIndex(0)
@@ -53,17 +53,23 @@ const ProfileDetails = (): JSX.Element => {
       <h1>Profile Details</h1>
       <p>{profile?.name}</p>
       <img src={profile?.photo} alt="Profile Photo" />
-      <div>
-        <h2>Favorite Movies</h2>
-        <p>Title: {movie?.name}</p>
-        <p>Released: {movie?.releaseDate}</p>
-      <button onClick={handleClick}>
-        Prev Movie
-      </button>
-      <button onClick={handleClick}>
-        Next Movie
-      </button>
-      </div>
+      {
+        movies?.length
+        ?
+        <div>
+          <h2>Favorite Movies</h2>
+          <p>Title: {movie?.name}</p>
+          <p>Released: {movie?.releaseDate}</p>
+          <button onClick={handleClick}>
+            Prev Movie
+          </button>
+          <button onClick={handleClick}>
+            Next Movie
+          </button>
+        </div>
+        :
+        <p>Add Movies</p>
+      }
     </>
   )
 }
