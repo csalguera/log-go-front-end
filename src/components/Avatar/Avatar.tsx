d3cf9df359613060b17ab781f5941df543ba7fce
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-27 17:45:13 -0500

    change sizing via inline-styling

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -7,6 +7,10 @@ import { AvatarProps } from '../../types/props'
 const Avatar = (props: AvatarProps) => {
   const { profile } = props
 
+  const size1 = "200px"
+  const size2 = "175px"
+  const size3 = "80px"
+
   const nameArr = profile!.name.split('')
   
   return (
@@ -15,9 +19,9 @@ const Avatar = (props: AvatarProps) => {
       ?
         <img src={profile?.photo} alt="Profile Photo" />
       :
-        <div className={styles["avatar-border"]}>
-          <div className={styles["nested-border"]}>
-            <h1>{nameArr[0]}</h1>
+        <div className={styles["avatar-border"]} style={{width: size1, height: size1}}>
+          <div className={styles["nested-border"]} style={{width: size2, height: size2}}>
+            <h1 style={{fontSize: size3}}>{nameArr[0]}</h1>
           </div>
         </div>
       }
