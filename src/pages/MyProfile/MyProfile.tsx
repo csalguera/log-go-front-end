c886f2d19b12ed2b1b9c2101a9e9776d77e3a7a7
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-28 12:05:28 -0500

    utilize useEffect to fetch user's profile

--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -1,6 +1,31 @@
+// npm packages
+import { useEffect, useState } from 'react'
+
+// services
+import * as profileService from '../../services/profileService'
+
+// types
+import { Profile } from '../../types/models'
+
 const MyProfile = (): JSX.Element => {
+  const [myProfile, setMyProfile] = useState<Profile | null>(null)
+  
+  useEffect(() => {
+    const fetchMyProfile = async (): Promise<void> => {
+      try {
+        const data: Profile = await profileService.getMyProfile()
+        setMyProfile(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchMyProfile()
+  }, [])
+
+  if (!myProfile) return <h1>Loading...</h1>
   return (
     <>
+      <p>{myProfile.name}</p>
     </>
   )
 }
