7352e1739aa099d92bc8b790ec8d577436c23c38
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-01 01:17:21 -0500

    add new ColorPicker component

---
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { ChromePicker, ColorResult } from 'react-color'
+
+
+const ColorPicker = (): JSX.Element => {
+  const [color, setColor] = useState('#000000')
+  
+  return (
+    <ChromePicker
+      color={color}
+      onChange={(newColor: ColorResult) => setColor(newColor.hex)}
+    />
+  )
+}
+
+export default ColorPicker
