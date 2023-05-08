5e82684858f03168bac771017ab703dc53ce2d43
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-07 23:06:25 -0400

    remove commented-out code

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,9 +1,7 @@
 // npm packages
 import { useState, useEffect } from 'react'
-// import { Link } from 'react-router-dom'
 
 // components
-// import Avatar from '../../components/Avatar/Avatar'
 import Loading from '../../components/Loading/Loading'
 
 // mui components
@@ -15,9 +13,6 @@ import Avatar from '@mui/material/Avatar'
 // services
 import * as profileService from '../../services/profileService'
 
-// styles
-import styles from './Profiles.module.css'
-
 // types
 import { Profile } from '../../types/models'
 
@@ -27,9 +22,6 @@ import { ProfilesProps } from '../../types/props'
 const Profiles = (props: ProfilesProps): JSX.Element => {
   const { user } = props
   const [profiles, setProfiles] = useState<Profile[]>([])
-  const size1 = "187px"
-  const size2 = "175px"
-  const size3 = "80px"
 
   useEffect((): void => {
     const fetchProfiles = async (): Promise<void> => {
@@ -90,24 +82,6 @@ const Profiles = (props: ProfilesProps): JSX.Element => {
           </Box>
         )}
       </Box>
-      {/* <h1>Profiles</h1>
-      <div className={styles["profiles-container"]}>
-        {profiles.filter((profile: Profile) => profile.id !== user!?.id).map((profile: Profile) =>
-          <Link
-            key={profile.id}
-            to={`/profiles/${profile.id}`}
-            className={styles["profile-link"]}
-          >
-            <Avatar
-              profile={profile}
-              size1={size1}
-              size2={size2}
-              size3={size3}
-            />
-            <p>{profile.name}</p>
-          </Link>
-        )}
-      </div> */}
     </main>
   )
 }
