d54485d4ae6443e298d05b8a533381e29a0fb8b4
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-14 13:41:58 -0400

    remove transform animation when editing movie

--- src/components/movies/MovieCard/MovieCard.tsx
+++ src/components/movies/MovieCard/MovieCard.tsx
@@ -38,6 +38,17 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(0deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const ExpandMoreAnimation = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
   marginLeft: 'auto',
   transition: theme.transitions.create('transform', {
@@ -217,7 +228,7 @@ const MovieCard = (props: MovieCardProps): JSX.Element => {
             mt: 2,
           }}
         >
-          <ExpandMore
+          <ExpandMoreAnimation
             expand={formDisplay}
             onClick={handleAdd}
             aria-expanded={formDisplay}
@@ -239,7 +250,7 @@ const MovieCard = (props: MovieCardProps): JSX.Element => {
             }}
           >
             <AddIcon />
-          </ExpandMore>
+          </ExpandMoreAnimation>
           <ExpandMore
             expand={editFormDisplay}
             onClick={handleEdit}
