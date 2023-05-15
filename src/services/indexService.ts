19101e1a88c460df9eca76a6f159470814d3ba66
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-14 20:45:56 -0400

    add service to fetch all resources

---
+++ src/services/indexService.ts
@@ -0,0 +1,22 @@
+// services
+import * as tokenService from './tokenService'
+
+// types
+import { Movie, Book } from '../types/models'
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/`
+
+async function index(): Promise<Movie[] | Book[]> {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return await res.json() as Movie[] | Book[]
+  } catch (error) {
+    throw error
+  }
+}
+
+export {
+  index,
+}
