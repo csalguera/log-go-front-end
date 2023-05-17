de6f18f7f42cb7c1be395acf8f3e8f37d2d328f6
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-17 19:00:52 -0400

    make BookCarousel component

---
+++ src/components/landing/BookCarousel/BookCarousel.tsx
@@ -0,0 +1,112 @@
+// npm modules
+import { useState, useEffect } from "react"
+
+// components
+import BookCardR from "../../books/BookCardR/BookCardR"
+
+// mui components
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+
+// types
+import { Book } from "../../../types/models"
+
+// props
+interface BookCarouselProps {
+  books: Book[];
+}
+
+const BookCarousel = (props: BookCarouselProps) => {
+  const { books } = props
+  const [bookscrollPosition, setBookScrollPosition] = useState(0)
+  const [bookscrollValue, setBookScrollValue] = useState(0)
+
+  useEffect(() => {
+    const setScroll = () => {
+      setBookScrollPosition(0)
+      setBookScrollValue(0)
+    }
+    setScroll()
+  }, [])
+
+  setTimeout(() => {
+    if (books.length > 3) handleBookRClick()
+  }, 3000);
+
+  function handleBookLClick(): void {
+    if (bookscrollPosition <= 0) {
+      setBookScrollPosition(books.length - 3)
+      setBookScrollValue((books.length - 3) *- 382)
+    } else {
+      setBookScrollPosition(bookscrollPosition - 1)
+      setBookScrollValue(bookscrollValue + 382)
+    }
+  }
+
+  function handleBookRClick(): void {
+    if (bookscrollPosition >= books.length - 3) {
+      setBookScrollPosition(0)
+      setBookScrollValue(0)
+    } else {
+      setBookScrollPosition(bookscrollPosition + 1)
+      setBookScrollValue(bookscrollValue - 382)
+    }
+  }
+
+  return (
+    <>
+      <Box
+        sx={{
+          width: 1146,
+          overflowX: 'hidden',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            width: 1164,
+            overflowX: 'hidden'
+          }}
+        >
+          <Box
+            sx={{
+              mt: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 600,
+              transform: bookscrollPosition ? `translate(${bookscrollValue}px)` : '',
+              transition: 'ease-in-out 1s',
+            }}
+          >
+            {books!?.map(book => (
+              <BookCardR
+                key={book.id}
+                book={book}
+              />
+            ))}
+          </Box>
+        </Box>
+      </Box>
+      <Box>
+        <Button
+          onClick={handleBookLClick}
+          disabled={books.length <= 3 || bookscrollPosition <= 0 ? true : false}
+          >
+          Left
+        </Button>
+        <Button
+          onClick={handleBookRClick}
+          disabled={books.length <= 3 || bookscrollPosition >= books.length - 3 ? true : false}
+        >
+          Right
+        </Button>
+      </Box>
+    </>
+  )
+}
+
+export default BookCarousel
--- src/components/landing/Home/Home.tsx
+++ src/components/landing/Home/Home.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 // components
 import MovieCardR from "../../movies/MovieCardR/MovieCardR"
 import BookCardR from "../../books/BookCardR/BookCardR"
+import BookCarousel from "../BookCarousel/BookCarousel"
 
 // mui components
 import Typography from "@mui/material/Typography"
@@ -23,8 +24,6 @@ const Home = (props: HomeProps) => {
   const { user } = props
   const [movies, setMovies] = useState<Movie[] | []>([])
   const [books, setBooks] = useState<Book[] | []>([])
-  const [bookscrollPosition, setBookScrollPosition] = useState(0)
-  const [bookscrollValue, setBookScrollValue] = useState(0)
 
   useEffect(() => {
     const fetchMovies = async (): Promise<void> => {
@@ -50,38 +49,6 @@ const Home = (props: HomeProps) => {
     fetchBooks()
   }, [])
 
-  useEffect(() => {
-    const setScroll = () => {
-      setBookScrollPosition(0)
-      setBookScrollValue(0)
-    }
-    setScroll()
-  }, [])
-
-  setTimeout(() => {
-    if (books.length > 3) handleBookRClick()
-  }, 3000);
-
-  function handleBookLClick(): void {
-    if (bookscrollPosition <= 0) {
-      setBookScrollPosition(books.length - 3)
-      setBookScrollValue((books.length - 3) *- 382)
-    } else {
-      setBookScrollPosition(bookscrollPosition - 1)
-      setBookScrollValue(bookscrollValue + 382)
-    }
-  }
-
-  function handleBookRClick(): void {
-    if (bookscrollPosition >= books.length - 3) {
-      setBookScrollPosition(0)
-      setBookScrollValue(0)
-    } else {
-      setBookScrollPosition(bookscrollPosition + 1)
-      setBookScrollValue(bookscrollValue - 382)
-    }
-  }
-
   return (
     <>
       <Typography
@@ -97,7 +64,7 @@ const Home = (props: HomeProps) => {
       >
         {movies && movies.length >= 5 ? 5 : movies.length} Most Recent Movie Submissions
       </Typography>
-      <Box
+      {/* <Box
         sx={{
           width: 1146,
           overflowX: 'hidden',
@@ -146,7 +113,7 @@ const Home = (props: HomeProps) => {
         >
           Right
         </Button>
-      </Box>
+      </Box> */}
       <Typography
         variant="h4"
         sx={{
@@ -155,56 +122,9 @@ const Home = (props: HomeProps) => {
       >
         {books && books.length >= 5 ? 5 : books.length} Most Recent Book Submissions
       </Typography>
-      <Box
-        sx={{
-          width: 1146,
-          overflowX: 'hidden',
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <Box
-          sx={{
-            width: 1164,
-            overflowX: 'hidden'
-          }}
-        >
-          <Box
-            sx={{
-              mt: 1,
-              display: 'flex',
-              flexDirection: 'column',
-              flexWrap: 'wrap',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: 600,
-              transform: bookscrollPosition ? `translate(${bookscrollValue}px)` : '',
-              transition: 'ease-in-out 1s',
-            }}
-          >
-            {books!?.map(book => (
-              <BookCardR
-                key={book.id}
-                book={book}
-              />
-            ))}
-          </Box>
-        </Box>
-      </Box>
-      <Box>
-        <Button
-          onClick={handleBookLClick}
-          disabled={books.length <= 3 || bookscrollPosition <= 0 ? true : false}
-          >
-          Left
-        </Button>
-        <Button
-          onClick={handleBookRClick}
-          disabled={books.length <= 3 || bookscrollPosition >= books.length - 3 ? true : false}
-        >
-          Right
-        </Button>
-      </Box>
+      <BookCarousel
+        books={books}
+      />
     </>
   )
 }
